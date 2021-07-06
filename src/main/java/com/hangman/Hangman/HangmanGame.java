a005e1e344c9fd23e6d4ec68ab9839ae6fa8bb3d
Julio Fuentes <>
2021-07-06 00:39:38 -0700

    Select random word from ArrayList and convert word into charArray

--- src/main/java/com/hangman/Hangman/HangmanGame.java
+++ src/main/java/com/hangman/Hangman/HangmanGame.java
@@ -10,17 +10,21 @@ public class HangmanGame {
     private ArrayList<String> wordList = new ArrayList<>();
     private Scanner textScanner;
     //maybe we should make the wordList static , we don't want every user making us run this operation a lot
+    //maybe make an init method that does the putting words into memory? honestly it should be done once.
 
     public HangmanGame() {
         try {
             //investigate classLoader later
-//          wordlist = new File(ClassLoader.getSystemClassLoader().getResource("../../../wordlist.txt").getFile());
+//          wordlistFile = new File(ClassLoader.getSystemClassLoader().getResource("../../../wordlist.txt").getFile());
             wordListFile = new File(this.getClass().getResource("../../../wordlist.txt").getFile());
             textScanner = new Scanner(wordListFile);
-            System.out.println("What words do we have here");
             while(textScanner.hasNextLine()) {
                 wordList.add(textScanner.nextLine());
             }
+            String selectedWord = wordList.get((int)(Math.random()* wordList.size()));
+            //when we have to reconstruct a game, we may have to extrapolate the next 2 lines into another method
+            char[] textArray = selectedWord.toCharArray();
+            char[] playerAnswers = new char[textArray.length];
         } catch (Exception e) {
             System.out.println(e);
         }
